3cabf488cfa907927b368aeb8c6f6bddb899e95e
Enes Akın Altuntaş <enesaltunts@gmail.com>
2024-03-25 13:37:02 +0300

    First commit

---
+++ ETicaretAPI.Persistence/DesingTimeDbContextFactory.cs
@@ -0,0 +1,22 @@
+﻿using ETicaretAPI.Persistence.Contexts;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ETicaretAPI.Persistence
+{
+    public class DesingTimeDbContextFactory : IDesignTimeDbContextFactory<ETicaretAPIDbContext>
+    {
+        public ETicaretAPIDbContext CreateDbContext(string[] args)
+        {
+            DbContextOptionsBuilder<ETicaretAPIDbContext> dbContextOptionsBuilder = new();
+            dbContextOptionsBuilder.UseNpgsql("User ID=postgres;Password=123456;Host=localhost;Port=5432;Database=ETicaretAPIDb;");
+            return new ETicaretAPIDbContext(dbContextOptionsBuilder.Options);
+        }
+    }
+}
